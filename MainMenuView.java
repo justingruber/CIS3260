257da4a724830be66ca0f1363a2b46e01af18745
Justin Gruber <justin.gruber12@gmail.com>
2014-09-30 23:23:33 -0400

    Adding base files for Main Menu and Game View.

---
+++ MainMenuView.java
@@ -0,0 +1,10 @@
+public abstract class MainMenuView {
+
+	/*public static void main(String[] args) {
+		System.out.println("test");	
+	}*/
+	
+	abstract void printBoard(); 
+	abstract void placePieces();
+	
+}
