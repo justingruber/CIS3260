0c6e50f670e321090bba3f8d74d59be9aaae9a1b
Justin Gruber <justin.gruber12@gmail.com>
2014-10-01 03:16:42 -0400

    Have two ways to display board, will add more depending on how practical I find differnt board types.

--- GameView.java
+++ GameView.java
@@ -1,25 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class GameView {
 
-	public static void main(String[] args) {
-		
-		int x = 0;
-		int y = 0;
 
-		for (x = 0; x < 8; x++){
-			System.out.print("___");
-		}
-		System.out.println();
-		for (x = 0; x < 8; x++) {
-			System.out.print("|");				
-
-			for(y = 0; y < 8; y++){
-				System.out.print("__|");
-			}
-			System.out.println();
-		}
-	}
-	
 	abstract void printBoard(); 
 	abstract void placePieces();
+	abstract void demoBoard();
 
 }
---
+++ VanillaBoardView.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VanillaBoardView extends GameView{
+	
+	public static void main(String[] args) {
+		VanillaBoardView newBoard = new VanillaBoardView();
+		newBoard.demoBoard();
+	}
+
+	@Override
+	public void placePieces(){
+
+	}
+
+	@Override
+	public void printBoard(){
+
+	}
+
+
+	@Override
+	public void demoBoard(){
+
+				
+		char tiles[] = new char[10*10+10];
+		Arrays.fill(tiles, '.');
+		for (int x = 0; x < 10; x++){
+			if(x == 0){
+				tiles[x] = '\u250F'; // Top left corner
+			}
+			else if(x > 1 || x < 8){
+				tiles[x] = '\u2501'; // Top bars
+				tiles[x*11] = '\u2503'; // Left bars
+				tiles[x*11+9] = '\u2503'; // Right bars
+				tiles[99] = '\u2517';	// Bottom left corner
+				tiles[108] = '\u251B'; // Bottom right corner
+				tiles[109-x] = '\u2501'; // Bottom row
+			}
+			if(x == 9){
+				tiles[x] = '\u2513'; // Top right corner
+			}
+			tiles[x*11+10] = '\n';
+		}
+
+
+		System.out.println(tiles);
+
+
+		/*int x = 0;
+		int y = 0;
+		for (x = 0; x < 8; x++){
+			System.out.print("___");
+		}
+		System.out.println();
+		for (x = 0; x < 8; x++) {
+			System.out.print("|");				
+
+			for(y = 0; y < 8; y++){
+				System.out.print("__|");
+			}
+			System.out.println();
+		}*/
+	}
+
+
+}
